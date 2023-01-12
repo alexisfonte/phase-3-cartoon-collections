d509feeefe4678b7d12d4c88aed3f860d836fbcd
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-12 01:46:16 -0600

    commit tree add85b4debd5e7e26b910c98caab23f87de58ca5

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call|
+    call.capitalize! << "!"
+  end
+  calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  items.find do |item|
+    cheese_types.include?(item)
+  end
 end
+
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+long_planeteer_calls(["two", "go", "industrious", "bop"])
